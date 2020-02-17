765562920816915f99ea1ee61bd06324735e7e4a
Stanley Huang <huang.stanley050@gmail.com>
2020-02-18 10:59:09 +1100

    added webpack base config

--- webpack.client.js
+++ webpack.client.js
@@ -1,8 +1,8 @@
 const path = require("path");
-// const merge = require('webpack-merge');
-// const baseConfig = require('./webpack.base.js');
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
 
-module.exports = {
+const clientConfig = {
   // Tell webpack the root file of our
   // server application
   entry: "./src/client/client.js",
@@ -12,23 +12,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { targets: { browsers: ["last 2 versions"] } }]
-          ]
-        }
-      }
-    ]
   }
 };
 
-//module.exports = merge(baseConfig, config);
+module.exports = merge(baseConfig, clientConfig);

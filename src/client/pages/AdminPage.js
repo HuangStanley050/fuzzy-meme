d894a2ec162b3cd0688fd1e9ed6201fcdd2e37c0
Stanley Huang <huang.stanley050@gmail.com>
2020-02-24 12:30:52 +1100

    admin page complete

--- src/client/pages/AdminPage.js
+++ src/client/pages/AdminPage.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchAdmins } from "../store/actions/adminActions";
 
-const Admin = () => {
-  return <h1>Admin page</h1>;
+const Admin = props => {
+  useEffect(() => {
+    props.getAdmins();
+  }, []);
+  const renderAdmins = () => {
+    return props.admins.map(admin => <li key={admin.id}>{admin.name}</li>);
+  };
+  return (
+    <div>
+      <h3>Protected list of admins</h3>
+      <ul>{renderAdmins()}</ul>
+    </div>
+  );
 };
-
+const mapState = state => ({
+  admins: state.admin
+});
+const mapDispatch = dispatch => ({
+  getAdmins: () => dispatch(fetchAdmins())
+});
 export default {
-  component: Admin
+  component: connect(
+    mapState,
+    mapDispatch
+  )(Admin),
+  loadData: ({ dispatch }) => dispatch(fetchAdmins())
 };

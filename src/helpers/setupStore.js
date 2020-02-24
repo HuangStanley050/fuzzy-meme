bd85074a285e2f94b5e718d1cf52ffbf0dcc4170
Stanley Huang <huang.stanley050@gmail.com>
2020-02-24 15:39:48 +1100

    fixed a bug at server side render admin page

--- src/helpers/setupStore.js
+++ src/helpers/setupStore.js
@@ -4,11 +4,13 @@ import axios from "axios";
 import API from "../api";
 import userReducer from "../client/store/reducers/user";
 import authReducer from "../client/store/reducers/auth";
+import adminReducer from "../client/store/reducers/admin";
 import { composeWithDevTools } from "remote-redux-devtools";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  auth: authReducer
+  auth: authReducer,
+  admin: adminReducer
 });
 export default req => {
   const axiosInstance = axios.create({
